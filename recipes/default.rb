e273969cf0219d9e0a2e1433d7968e2aab4c8170
Nelson Chen <crazysim@gmail.com>
2013-01-17 16:06:07 -0800

    try adding user

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,6 +3,14 @@
 # Recipe:: default
 #
 # Copyright (C) 2013 YOUR_NAME
-# 
+#
 # All rights reserved - Do Not Redistribute
 #
+
+group 'hlds'
+
+user "hlds" do
+  group "hlds"
+  system true
+  shell "/bin/bash"
+end
