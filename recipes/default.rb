3d82d4a812cea9ca1bd3fe65801e1cb908d91f71
Nelson Chen <crazysim@gmail.com>
2013-02-03 15:01:02 -0800

    pull remote file

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,3 +13,9 @@ user "hlds" do
   group "hlds"
   shell "/bin/bash"
 end
+
+remote_file "/opt/hlds/hldsupdatetool.bin" do
+  source "http://storefront.steampowered.com/download/hldsupdatetool.bin"
+  # This file hasn't changed since 2005
+  action :create_if_missing
+end
