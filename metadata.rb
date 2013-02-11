68cc1778708a0675c38b4a6a812daac750054913
Nelson Chen <crazysim@gmail.com>
2013-02-11 15:17:07 -0800

    update metadata

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,8 @@
 name             "steam-ds"
-maintainer       "YOUR_NAME"
-maintainer_email "YOUR_EMAIL"
+maintainer       "Nelson Chen"
+maintainer_email "crazysim@gmail.com"
 license          "All rights reserved"
-description      "Installs/Configures steam-ds"
+description      "Installs/Configures steam"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
-depends          "user"
 
