e25786ea3f1a941541021ef33b147af9288d0009
Nelson Chen <crazysim@gmail.com>
2013-02-14 22:25:55 -0800

    rough supervisor idea

--- metadata.rb
+++ metadata.rb
@@ -6,4 +6,5 @@ description      "Installs/Configures steam"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.1.0"
 depends          "tmux"
+depends          "supervisor"
 
--- recipes/hl.rb
+++ recipes/hl.rb
@@ -1,9 +1,17 @@
 # Can't believe you're installing this.
 
 include_recipe "tmux"
+include_recipe "supervisor"
 
 execute "#{node['steam']['dir']}/steam -command update -game valve -dir valve" do
   user node['steam']['user']
   cwd node['steam']['dir']
+end
+
+supervisor_service "valve" do
+  action :enable
+  directory "#{node['steam']['dir']}/valve"
+  command "#{node['steam']['dir']}/valve/hlds_run"
+  user node['steam']['user']
 end
 
